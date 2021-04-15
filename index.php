8410222b59b3d98daa31bf96a738619d77f8c8cb
Vlad Aksenov <vaksenov@mail.greenriver.edu>
2021-04-15 12:45:49 -0400

    Created php file

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+// Vlad Aksenov
+// April 15, 2021
+// http://vaksenov.greenriverdev.com/328/pp2/index.php
+// PP2 - Arrays
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Array Practice</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+<?php
+
+    error_reporting(E_ALL);
+    echo "<h1>PHP Array Practice</h1>";
+
+
+    
+?>
+
+
